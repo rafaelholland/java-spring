47a006e6dce9fc240019a06923e90ef7e6dec750
rafaelholland <rafael.holland@bandtec.com.br>
2021-04-22 23:56:47 -0300

    Testando a primeira classe de cominio -categoria

--- src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.rafael.cursomc.resources;
 
+import com.rafael.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-            return "o rest esta funcionando";
-        }
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+
+    }
 }
