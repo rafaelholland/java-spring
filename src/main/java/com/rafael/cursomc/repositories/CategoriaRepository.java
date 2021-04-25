a1e6208ffc1c06f51d6e5b20dd58f3d73d66703c
rafaelholland <rafael.holland@bandtec.com.br>
2021-04-25 14:35:00 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/rafael/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.rafael.cursomc.repositories;
+
+
+import com.rafael.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+
+
+
+
+}
